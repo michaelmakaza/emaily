e88ac38bb3c9011782e5346adc354bc9b3633b07
Michael F. Makaza <Michael.Makaza@econet.co.zw>
2026-07-18 19:26:26 +0200

    fix: enable proxy option for passport OAuth strategy

--- services/passport.js
+++ services/passport.js
@@ -22,6 +22,7 @@ passport.use(
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
       callbackURL: '/auth/google/callback'
+      proxy: true // <-- CRITICAL line for Render/Heroku hosting
     },
     (accessToken, refreshToken, profile, done ) => {
       User.findOne({googleId: profile.id})
